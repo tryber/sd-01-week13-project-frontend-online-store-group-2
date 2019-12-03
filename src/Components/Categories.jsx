af70eca2e9ddbfc690391b91a3f7bb3d3a283203
Guilherme Luiz <guiiluiz44@gmail.com>
2019-12-03 18:55:05 -0300

    resolve erros codeclimate

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import * as ProductAPI from '../Services/ProductAPI';
-import Loading from './Loading';
 
 class Categories extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: true,
       categories: [],
     };
   }
@@ -14,23 +12,19 @@ class Categories extends Component {
   componentDidMount() {
     ProductAPI.getCategories()
       .then((data) => this.setState({
-        loading: false,
         categories: data,
       }));
-  }  
+  }
 
   render() {
-    if (loading) return <Loading />;
-    const { categories, loading } = this.state;
-    const createRadioButton = (name, id) => {
-      return (
-        <label htmlFor="categories">
-          {name}
-          <input type="radio" name="categories" value={id} />
-        </label>
-      );
-    }
-    
+    const { categories } = this.state;
+    const createRadioButton = (name, id) => (
+      <label htmlFor={id}>
+        <input type="radio" id={id} name="categories" value={id} />
+        {name}
+      </label>
+    );
+
     return (
       categories.map(({ name, id }) => createRadioButton(name, id))
     );
