384014be018ea845306c98f457521b3f780840d7
DougFunny <douglaseduardo220483@gmail.com>
2019-12-04 15:14:02 -0300

    Colocando a div com o dados

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,2 +1,31 @@
 // Componentes presentes:
 // - ProductCard.jsx
+import React, { Component } from "react";
+import ProductCard from "./ProductCard";
+import * as ProductAPI from "../Services/ProductAPI";
+
+export default class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dados: []
+    };
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    const frase = 'Busca vazia!'
+    const { dados } = this.state;
+    if (dados.length === 0) return frase
+    return (
+      <div className="product-list">
+        {dados.map(product => (
+          <ProductCard item={product} />
+        ))}
+      </div>
+    );
+  }
+}
