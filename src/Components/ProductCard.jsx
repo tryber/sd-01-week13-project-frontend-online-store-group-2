fc275ca19c9c572d8ebb1ceb2dcc139f72e6e459
Henrique Eyer <henriqueeyer@outlook.com>
2019-12-03 23:18:49 -0300

    Passando no code cliamte adicionando porptypes

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import * as LocalStorageAPI from '../Services/LocalStorageAPI';
 
 class ProductCard extends Component {
   render() {
-    const { price, title, thumbnail } = this.props;
+    const { price, title, thumbnail } = this.props.item;
     return (
       <div className="card-product">
         <div className="title">
           <h3>{title}</h3>
         </div>
         <div className="info-product">
-          <img className="img-product" src={thumbnail} />
+          <img className="img-product" alt="imagem do produto" src={thumbnail} />
           <p className="value">{`R$ ${price}`}</p>
           <button type="button" onClick={LocalStorageAPI.addCart(/*values*/)}>Adicionar Item</button>
         </div>
@@ -20,8 +21,11 @@ class ProductCard extends Component {
 }
 
 export default ProductCard;
-//FALTA SABER ONDE SERÁ ADICIONADO O LINK PARA VER MAIS
-//OS DETALHES DO PRODUTO E FUNÇÃO PARA ADICIONAR NOVO ITEM NO CARRINHO
-//FUNC SERÁ FEITA NO LOCALSTORAGEAPI. BASTARÁ APENAS PASSA-LA PARA O ONCLICK 
-//NO BOTAO
-//thumbnail é o caminho da imagem, nome que está na api é esse
+
+ProductCard.propTypes = {
+  item: PropTypes.shape({
+    price: PropTypes.number,
+    title: PropTypes.string,
+    thumbnail: PropTypes.string,
+  }).isRequired,
+};
