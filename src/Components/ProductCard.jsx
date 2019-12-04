87c39ecc2491836e20cd5a776ca6450bbb03f724
Henrique Eyer <henriqueeyer@outlook.com>
2019-12-03 23:04:57 -0300

    Terminando base do componente Product

---
+++ src/Components/ProductCard.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import * as LocalStorageAPI from '../Services/LocalStorageAPI';
+
+class ProductCard extends Component {
+  render() {
+    const { price, title, thumbnail } = this.props
+    return (
+      <div className = "card-product">
+        <div className = "title">
+          <h3>{title}</h3>
+        </div>
+        <div className = "info-product">
+          <img className = "img-product" src = {thumbnail} />
+          <p className = "value">{`R$ ${price}`}</p>
+          <button type="button" onClick={LocalStorageAPI.addCart(/*values*/)}></button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProductCard;
+// FALTA SABER ONDE SERÁ ADICIONADO O LINK PARA VER MAIS
+// OS DETALHES DO PRODUTO E FUNÇÃO PARA ADICIONAR NOVO ITEM NO CARRINHO
+// FUNC SERÁ FEITA NO LOCALSTORAGEAPI. BASTARÁ APENAS PASSA-LA PARA O ONCLICK 
+// NO BOTAO
+
+//thumbnail é o caminho da imagem, nome que está na api é esse
