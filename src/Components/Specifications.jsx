846db28b1f6fee6b79ccf30af9653c8d9a057c06
Guilherme Luiz <guiiluiz44@gmail.com>
2019-12-04 17:49:54 -0300

    começa detalhes do produto

---
+++ src/Components/Specifications.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export class Specifications extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Specifications;
--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -2,3 +2,31 @@
 // - Specifications.jsx
 // - CreateRating.jsx
 // - RatingComments.jsx
+
+import React, { Component } from 'react';
+import Specifications from '../Components/Specifications';
+
+export class ProductDetails extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          {this.props.thumbnail}
+        </div>
+        <div>
+          <Specifications product={this.props}/>
+        </div>
+        <Link to="/shopcart"/>
+      </div>
+    );
+  }
+}
+
+export default ProductDetails;
