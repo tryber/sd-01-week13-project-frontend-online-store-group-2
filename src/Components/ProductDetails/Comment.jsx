b0ea90625953a303161687cabb89f8177cac680a
DougFunny <douglaseduardo220483@gmail.com>
2019-12-05 19:13:30 -0300

    Criando o componente star

---
+++ src/Components/ProductDetails/Comment.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import Star from '../ProductDetails/Star';
+export default class Comment extends Component {
+  render() {
+    return <Star />;
+  }
+}
---
+++ src/Components/ProductDetails/Star.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import StarRatings from 'react-star-ratings';
+export class Star extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rating: 0,
+    };
+    this.changeRating = this.changeRating.bind(this);
+  }
+  changeRating(newRating) {
+    console.log(newRating);
+    this.setState({
+      rating: newRating,
+    });
+  }
+  render() {
+    return (
+      <StarRatings
+        rating={this.state.rating}
+        starRatedColor="blue"
+        changeRating={this.changeRating}
+        numberOfStars={6}
+      />
+    );
+  }
+}
+export default Star;
