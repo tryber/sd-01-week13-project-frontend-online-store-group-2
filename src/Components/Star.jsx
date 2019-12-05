7b3269bb7eac78a3950be84cfe5aa19bf4659ac9
DougFunny <douglaseduardo220483@gmail.com>
2019-12-05 18:45:32 -0300

    Mais algumas alterações

--- src/Components/Star.jsx
+++ src/Components/Star.jsx
@@ -1,36 +1,33 @@
 import React, { Component } from 'react';
 import StarRatings from 'react-star-ratings';
+
 export class Star extends Component {
-constructor(props) {
-  super(props)
+  constructor(props) {
+    super(props);
+    this.state = {
+      rating: 0,
+    };
+    this.changeRating = this.changeRating.bind(this);
+  }
 
-  this.state = {
-    rating: 0,
-  };
-  this.changeRating = this.changeRating.bind(this)
-};
 
-
-  changeRating( newRating ) {
+  changeRating(newRating) {
     console.log(newRating);
     this.setState({
-      rating: newRating
+      rating: newRating,
     });
   }
 
   render() {
-   
     return (
       <StarRatings
         rating={this.state.rating}
         starRatedColor="blue"
         changeRating={this.changeRating}
-        numberOfStars={6}
-        
+        numberOfStars={6}    
       />
     );
   }
 }
 
-export default Star
-
+export default Star;
