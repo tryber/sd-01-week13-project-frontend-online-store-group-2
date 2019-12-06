e9fcb54a0766ed20c311d41e9837356cf4cc6763
Henrique Eyer <henriqueeyer@outlook.com>
2019-12-06 17:34:13 -0300

    Partiu code climate monstrao

--- src/Services/LocalStorageAPI.jsx
+++ src/Services/LocalStorageAPI.jsx
@@ -14,22 +14,22 @@ export const removeItem = (id) => {
 }
 
 export const UpdateItemQtd = (id, value) => {
-  const item=getItem(id);
-  item.qtd=value;
+  const item = getItem(id);
+  item.qtd = value;
   setNewItem(item);
 }
 
 export const AllItens = () => {
   console.log(Object.keys(localStorage))
-  const arrKeys = Object.keys(localStorage).filter((keys)=>keys.includes('MLB'));
-  const itens = arrKeys.map((key)=>getItem(key));
+  const arrKeys = Object.keys(localStorage).filter((keys) => keys.includes('MLB'));
+  const itens = arrKeys.map((key) => getItem(key));
   return itens;
 }
 
 export const valueTotal = () => {
-  return AllItens().reduce((acc,item)=> {
-    let total=acc;
-    total+=item.price*item.qtd;
+  return AllItens().reduce((acc, item) => {
+    let total = acc;
+    total += item.price * item.qtd;
     return total;
-  },0);
+  }, 0);
 }
