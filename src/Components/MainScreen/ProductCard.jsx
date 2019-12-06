8676e6fcd050ac16c79940700359fecff4080c1c
Henrique Eyer <henriqueeyer@outlook.com>
2019-12-06 17:54:15 -0300

    Partiu code climate monstrao

--- src/Components/MainScreen/ProductCard.jsx
+++ src/Components/MainScreen/ProductCard.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import * as LocalStorageApi from '../../Services/LocalStorageAPI';
-import '../../Style/ProductCard.css'
+import '../../Style/ProductCard.css';
 
 
 class ProductCard extends Component {
@@ -13,6 +13,7 @@ class ProductCard extends Component {
     };
 
     this.toggle = this.toggle.bind(this);
+    this.createButton = this.createButton.bind(this);
   }
 
   componentDidUpdate() {
@@ -20,12 +21,10 @@ class ProductCard extends Component {
     const { price, title, thumbnail, id, available_quantity } = item;
     const obj = { id, price, title, thumbnail, available_quantity, qtd: 1 };
     if (this.state.added) {
-      console.log('adicionado')
       LocalStorageApi.setNewItem(obj);
     } else {
-      const value = LocalStorageApi.getItem(id)
+      const value = LocalStorageApi.getItem(id);
       if (value !== null) {
-        console.log('removido')
         LocalStorageApi.removeItem(id);
       }
     }
@@ -35,11 +34,27 @@ class ProductCard extends Component {
     this.setState({ added: !this.state.added });
   }
 
+  createButton(value) {
+    if (value) {
+      return (
+        <span onClick={this.toggle} className="buttonAddCart" >
+          Adicionar Item
+      </span>
+      )
+    } else {
+      return (
+        <span onClick={this.toggle} className="buttonRemoveCart" >
+          remover Item
+      </span>
+      )
+    }
+  }
+
   render() {
     const { item } = this.props;
     const { price, title, thumbnail, id } = item;
-    let cardClass = ["card-product"];
-    let titleSpace = title.split(' ');
+    const cardClass = ["card-product"];
+    const titleSpace = title.split(' ');
     if (this.state.added) {
       cardClass.push('border');
     }
@@ -55,12 +70,7 @@ class ProductCard extends Component {
           <div className="info-product">
             <img className="img-product" alt="imagem do produto" src={thumbnail} />
           </div>
-          {!this.state.added && <span onClick={this.toggle.bind(this)} className="buttonAddCart" >
-            Adicionar Item
-           </span>}
-          {this.state.added && <span onClick={this.toggle.bind(this)} className="buttonRemoveCart" >
-            remover Item
-           </span>}
+          {this.createButton(this.state.added)}
           <Link to={{ pathname: `products/${id}`, state: { productDetails: item } }}>
             DETALHES
           </Link>
@@ -69,41 +79,6 @@ class ProductCard extends Component {
     );
   }
 }
-
-// this.state = {addClass: false}
-// }
-// toggle() {
-//   this.setState({addClass: !this.state.addClass});
-// }
-// render() {
-//   const { price, title, thumbnail } = this.props.item;
-//   let titleSpace = title.split(' ');
-//   let cardClass = ["card-product"];
-//   if(this.state.addClass) {
-//     cardClass.push('border');
-//   }
-//   return (
-//     <section className="content-center">
-//       <div className={cardClass.join(' ')}>
-//         <div className="title">
-//           <h3>{`${titleSpace[0]} ${titleSpace[1]}`}</h3>
-//           <h3>{`${titleSpace[2]} ${titleSpace[3]}`}</h3>
-//           <p className="value">{`R$ ${price}`}</p>
-//         </div>
-//         <div className="info-product">
-//           <img className="img-product" alt="imagem do produto" src={thumbnail} />
-//         </div>
-//           <span onClick={this.toggle.bind(this)} className="buttonAddCart" >
-//             Adicionar Item
-//           </span>
-//           <span onClick={this.toggle.bind(this)} className="buttonRemoveCart" >
-//             remover Item
-//           </span>
-//       </div>
-//     </section>
-//   );
-// }
-// }
 
 export default ProductCard;
 
@@ -115,7 +90,3 @@ ProductCard.propTypes = {
     thumbnail: PropTypes.string,
   }).isRequired,
 };
-
-// Tênis Feminino Casual Sapatênis Original Chiquiteira 60106
-// 44.99
-// http://mlb-s2-p.mlstatic.com/870455-MLB31209566380_062019-I.jpg
