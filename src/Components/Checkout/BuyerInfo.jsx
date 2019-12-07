90402815c067d0a42890c32374102d3e6c4641e5
Douglas Eduardo <douglaseduardo220483@gmail.com>
2019-12-07 14:20:05 -0300

    alterações no cc

--- src/Components/Checkout/BuyerInfo.jsx
+++ src/Components/Checkout/BuyerInfo.jsx
@@ -1,40 +1,40 @@
 import React, { Component } from 'react';
 
-const estados = [
-  <option value="AC">Acre</option>,
-  <option value="AL">Alagoas</option>,
-  <option value="AP">Amapá</option>,
-  <option value="AM">Amazonas</option>,
-  <option value="BA">Bahia</option>,
-  <option value="CE">Ceará</option>,
-  <option value="DF">Distrito Federal</option>,
-  <option value="ES">Espírito Santo</option>,
-  <option value="GO">Goiás</option>,
-  <option value="MA">Maranhão</option>,
-  <option value="MT">Mato Grosso</option>,
-  <option value="MS">Mato Grosso do Sul</option>,
-  <option value="MG">Minas Gerais</option>,
-  <option value="PA">Pará</option>,
-  <option value="PB">Paraíba</option>,
-  <option value="PR">Paraná</option>,
-  <option value="PE">Pernambuco</option>,
-  <option value="PI">Piauí</option>,
-  <option value="RJ">Rio de Janeiro</option>,
-  <option value="RN">Rio Grande do Norte</option>,
-  <option value="RS">Rio Grande do Sul</option>,
-  <option value="RO">Rondônia</option>,
-  <option value="RR">Roraima</option>,
-  <option value="SC">Santa Catarina</option>,
-  <option value="SP">São Paulo</option>,
-  <option value="SE">Sergipe</option>,
-  <option value="TO">Tocantins</option>,
-  <option value="EX">Estrangeiro</option>
-];
+const estadosBrasileiros = {
+  Acre: 'AC',
+  Alagoas: 'AL',
+  Amapá: 'AP',
+  Amazonas: 'AM',
+  Bahia: 'BA',
+  Ceará: 'CE',
+  'Distrito Federal': 'DF',
+  'Espírito Santo': 'ES',
+  Goiás: 'GO',
+  Maranhão: 'MA',
+  'Mato Grosso': 'MT',
+  'Mato Grosso do Sul': 'MS',
+  'Minas Gerais': 'MG',
+  Pará: 'PA',
+  Paraíba: 'PB',
+  Paraná: 'PR',
+  Pernambuco: 'PE',
+  Piauí: 'PI',
+  'Rio de Janeiro': 'RJ',
+  'Rio Grande do Norte': 'RN',
+  'Rio Grande do Sul': 'RS',
+  Rondônia: 'RO',
+  Roraima: 'RR',
+  'Santa Catarina': 'SC',
+  'São Paulo': 'SP',
+  Sergipe: 'SE',
+  Tocantins: 'TO',
+  Estrangeiro: 'EX',
+};
 export class BuyerInfo extends Component {
   static estadosBrasileiros() {
     return (
-      <select id="estado" name="Estado">
-        {estados.map(select => select)}
+      <select id='estado' name='Estado'>
+        {estados.map((select) => select)}
       </select>
     );
   }
@@ -43,7 +43,15 @@ export class BuyerInfo extends Component {
   }
 
   static createInputSelect() {
-    return <div>{BuyerInfo.estadosBrasileiros}</div>;
+    return (
+      <div>
+        <select name='Estados' id='estados-brasileiros'>
+          {Object.keys(estadosBrasileiros).forEach((key) => {
+            return <option value={estadosBrasileiros[key]}>{key}</option>;
+          })}
+        </select>
+      </div>
+    );
   }
   constructor(props) {
     super(props);
@@ -55,10 +63,10 @@ export class BuyerInfo extends Component {
 
   render() {
     return (
-      <div className="input-data-cliente">
+      <div className='input-data-cliente'>
         <span>Informações do Comprador</span>
         <div>
-          {BuyerInfo.createInput("text", "Nome Completo", "nome-usuário")}
+          {BuyerInfo.createInput('text', 'Nome Completo', 'nome-usuário')}
         </div>
         <div>{BuyerInfo.createInput('number', 'CPF', 'CPF')}</div>
         <div>{BuyerInfo.createInput('email', 'Email', 'email')}</div>
