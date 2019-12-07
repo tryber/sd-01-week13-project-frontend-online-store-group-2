e58d322e47e57aaa9581840c98df1f7248abc00d
Douglas Eduardo <douglaseduardo220483@gmail.com>
2019-12-07 13:48:36 -0300

    CC de novo

--- src/Components/Checkout/BuyerInfo.jsx
+++ src/Components/Checkout/BuyerInfo.jsx
@@ -1,39 +1,40 @@
 import React, { Component } from 'react';
 
+const estados = [
+  <option value="AC">Acre</option>,
+  <option value="AL">Alagoas</option>,
+  <option value="AP">Amapá</option>,
+  <option value="AM">Amazonas</option>,
+  <option value="BA">Bahia</option>,
+  <option value="CE">Ceará</option>,
+  <option value="DF">Distrito Federal</option>,
+  <option value="ES">Espírito Santo</option>,
+  <option value="GO">Goiás</option>,
+  <option value="MA">Maranhão</option>,
+  <option value="MT">Mato Grosso</option>,
+  <option value="MS">Mato Grosso do Sul</option>,
+  <option value="MG">Minas Gerais</option>,
+  <option value="PA">Pará</option>,
+  <option value="PB">Paraíba</option>,
+  <option value="PR">Paraná</option>,
+  <option value="PE">Pernambuco</option>,
+  <option value="PI">Piauí</option>,
+  <option value="RJ">Rio de Janeiro</option>,
+  <option value="RN">Rio Grande do Norte</option>,
+  <option value="RS">Rio Grande do Sul</option>,
+  <option value="RO">Rondônia</option>,
+  <option value="RR">Roraima</option>,
+  <option value="SC">Santa Catarina</option>,
+  <option value="SP">São Paulo</option>,
+  <option value="SE">Sergipe</option>,
+  <option value="TO">Tocantins</option>,
+  <option value="EX">Estrangeiro</option>
+];
 export class BuyerInfo extends Component {
   static estadosBrasileiros() {
     return (
       <select id="estado" name="Estado">
-        {[
-          <option value="AC">Acre</option>,
-          <option value="AL">Alagoas</option>,
-          <option value="AP">Amapá</option>,
-          <option value="AM">Amazonas</option>,
-          <option value="BA">Bahia</option>,
-          <option value="CE">Ceará</option>,
-          <option value="DF">Distrito Federal</option>,
-          <option value="ES">Espírito Santo</option>,
-          <option value="GO">Goiás</option>,
-          <option value="MA">Maranhão</option>,
-          <option value="MT">Mato Grosso</option>,
-          <option value="MS">Mato Grosso do Sul</option>,
-          <option value="MG">Minas Gerais</option>,
-          <option value="PA">Pará</option>,
-          <option value="PB">Paraíba</option>,
-          <option value="PR">Paraná</option>,
-          <option value="PE">Pernambuco</option>,
-          <option value="PI">Piauí</option>,
-          <option value="RJ">Rio de Janeiro</option>,
-          <option value="RN">Rio Grande do Norte</option>,
-          <option value="RS">Rio Grande do Sul</option>,
-          <option value="RO">Rondônia</option>,
-          <option value="RR">Roraima</option>,
-          <option value="SC">Santa Catarina</option>,
-          <option value="SP">São Paulo</option>,
-          <option value="SE">Sergipe</option>,
-          <option value="TO">Tocantins</option>,
-          <option value="EX">Estrangeiro</option>
-        ].map(select => select)}
+        {estados.map(select => select)}
       </select>
     );
   }
@@ -57,7 +58,7 @@ export class BuyerInfo extends Component {
       <div className="input-data-cliente">
         <span>Informações do Comprador</span>
         <div>
-          {BuyerInfo.createInput('text', 'Nome Completo', 'nome-usuário')}
+          {BuyerInfo.createInput("text", "Nome Completo", "nome-usuário")}
         </div>
         <div>{BuyerInfo.createInput('number', 'CPF', 'CPF')}</div>
         <div>{BuyerInfo.createInput('email', 'Email', 'email')}</div>
